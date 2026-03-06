c6d7345235c620474f9bbfdbe6ec30b05cd886dd
kodurin407-wq <kodurin407@gmail.com>
2026-03-06 14:15:24 +0530

    Add files via upload
    positive and negative numbers with return and without return

---
+++ +ve and -ve numbers using with return & arguments.cpp
@@ -0,0 +1,20 @@
+//+ve and -ve numbers using with return & arguments
+#include<stdio.h>
+int numbers(int);
+int main()
+{
+	int n;
+	printf("enter a number");
+	scanf("%d",&n);
+	if ("number(n)")
+	printf("it is a positive number");
+	else
+	printf("it is a negative number");
+}
+ int number(int x)
+ {
+ 	if (x>0)
+ 	return 1;
+ 	else
+ 	return 0;
+ }
